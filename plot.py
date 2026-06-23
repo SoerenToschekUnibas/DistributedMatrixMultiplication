5bc9b9331272dcef369472bb31c2ff1d64b795fa
SoerenToschekUnibas <sorentoschekunibas@gmail.com>
2026-06-23 10:19:11 +0200

    file for plotting.

---
+++ plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+
+data_dict = {}
+#open file after another for the plot
+
+
+
+
+plt.xlabel("Configuration")
+plt.ylabel("End-to-end execution time [s]")
+plt.show()
